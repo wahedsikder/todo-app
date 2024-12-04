d58135faf76a6b9e7b25e64e0c414e65e65665f6
Wahed Sikder <wahedsikder@gmail.com>
2024-12-04 19:18:08 +0600

    first commit

---
+++ src/components/TodoButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <button>
+            <slot>Create</slot>
+        </button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+button {
+        padding: 8px 16px;
+        border: none;
+    }
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is a Todo App built with Vue</h1>
+  </div>
+</template>
+
+<style>
+.about {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+h1 {
+  font-size: 2rem;
+  color: #41b080;
+}
+</style>
